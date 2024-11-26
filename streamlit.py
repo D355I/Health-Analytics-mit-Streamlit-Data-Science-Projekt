b2d25e1a6ad128293dc7dc88695e770c3bb9cb28
D355I <jonabach.kontakt@gmail.com>
2024-11-26 11:11:47 +0100

    anfang

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly as px
+
+
+
+col1, col2, col3 = st.columns([1,2,1])
+
+col1.markdown("DATA LITERACY ANALYTICAL PROJECT")
+
+file = col2.file_uploader("Upload your file here")
 
