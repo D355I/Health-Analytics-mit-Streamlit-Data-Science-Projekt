be439017a11a599fc4f0384b9c4e1e18092d5504
D355I <jonabach.kontakt@gmail.com>
2025-01-22 12:57:10 +0100

    streamlit

---
+++ code/streamlit/Info.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.set_page_config(page_title = "Data Literacy Dashboard",layout = "wide")
+st.sidebar.image("./assets/logo-TH-Köln1.png",caption = "Data Literacy")
+scope = pd.read_excel("./assets/scope.xlsx", index_col=None)
+st.sidebar.write(scope)
+
+#tode_2019 = pd.read_excel("../../data/cleaned_data/data_tode_2019_final.xlsx")
+
+
+
+
+st.header("Data Literacy Dashboard")
+st.write(""" Das folgende Projekt im Wahlpflichtfach “Data Literacy” untersucht Unterschiede, Zusammenhänge und
+Entwicklungen sowie Ursachen der variierenden Sterblichkeitsraten und Todesfälle in deutschen
+Krankenhäusern. Dabei steht im Fokus, ob diese Faktoren durch spezifische Ursachen wie zum Beispiel
+Krankheiten, Behandlungsmethoden oder Krankenhausbedingungen beeinflusst werden.
+.""")
+
+st.subheader("Datengrundlage:")
+st.write("Als Rohdaten hatten wir eine Basis von 4 Tabellen zur VErfügung. Diese wurden aus Destatis herausgezogen:")
+#st.write(tode_2019.head(10))ç
---
+++ code/streamlit/pages/Zusammenhänge.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.set_page_config(page_title = "Data Literacy Dashboard",layout = "wide")
+st.sidebar.image("./assets/logo-TH-Köln1.png",caption = "Data Literacy")
