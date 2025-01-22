a4d1a53962c6a9aa68c99c5e4ac99c1736613701
D355I <jonabach.kontakt@gmail.com>
2025-01-22 17:43:04 +0100

    functions

--- code/streamlit/functions.py
+++ code/streamlit/functions.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import streamlit as st
+from get_data import load_data
+
 
 def interactive_plot_dev(df,x, spalte_von, spalte_bis):
     x_axis_val = df.columns[x]
@@ -10,10 +12,32 @@ def interactive_plot_dev(df,x, spalte_von, spalte_bis):
     plot = px.scatter(df, x = x_axis_val, y= y_axis_val)
     st.plotly_chart(plot)
 
-def columns(df,x, zeile_von, zeile_bis, text):
+def columns_one_graph(df,x, zeile_von, zeile_bis, text):
     col1, col2 = st.columns(2)
 
     with col1: 
         interactive_plot_dev(df,x, zeile_von, zeile_bis)
     with col2: 
         st.write(text)
+
+def interactive_plot_bar(df, y, spalte_von, spalte_bis):
+    y_axis_val = df.columns[y]
+    x_axis_val = st.selectbox("Optionen", options=df.columns[spalte_von:spalte_bis])
+
+    plot = px.bar(df, x = x_axis_val, y= y_axis_val)
+    st.plotly_chart(plot)
+
+def columns_graph(df,x, zeile_von, zeile_bis, df2, x_2, zeile_von2, zeile_bis2):
+    col1, col2 = st.columns(2)
+
+    with col1: 
+        interactive_plot_dev(df,x, zeile_von, zeile_bis)
+    with col2: 
+        interactive_plot_dev(df2,x_2, zeile_von2, zeile_bis2)
+
+def plot_dev(df, x, y):
+    x_axis = df.columns[x]
+    y_axis = df.columns[y]
+    plot = px.scatter(df, x=x_axis, y=y_axis)
+    return plot
+
--- code/streamlit/pages/Entwicklung.py
+++ code/streamlit/pages/Entwicklung.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from get_data import load_data
-from functions import interactive_plot_dev, columns
+from functions import interactive_plot_dev, columns_one_graph, interactive_plot_bar, columns_graph, plot_dev
 
 
 st.set_page_config(page_title = "Data Literacy Dashboard",layout = "wide")
@@ -16,10 +16,20 @@ d_kh, d_pn, d_dg2018, d_dg2019, d_dg2020, d_dg2021, d_dg2022, d_dg2023 = load_da
 st.header("Entwicklung")
 st.subheader("Entwicklung bei den Krankenhäusern")
 
-interactive_plot_dev(d_dg2018,0, 1, 5)
+#interactive_plot_bar(d_dg2018,0, 2, 7)
 
 st.subheader("Entwicklung bei dem Personal")
 
-interactive_plot_dev(d_pn,0, 1, 5)
+# interactive_plot_dev(d_pn,0, 1, 5)
 
-columns(d_kh,0, 1, 5, "Hallo Welt")
+#columns(d_kh,0, 1, 5, "Hallo Welt")
+
+# columns_graph(d_kh, 0, 1, 5, d_pn, 0, 1, 5)
+
+
+plot1 = plot_dev(d_kh, 0, 1)
+plot2 = plot_dev(d_pn, 0, 1)
+
+col1 , col2 = st.columns(2)
+col1.write(plot1)
+col2.write(plot2)
