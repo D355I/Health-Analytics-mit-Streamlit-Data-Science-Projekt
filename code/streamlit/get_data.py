5f6a10002a35fb1d5cff433aa9e1501b76cea74f
D355I <jonabach.kontakt@gmail.com>
2025-01-24 10:07:01 +0100

    all data

--- code/streamlit/get_data.py
+++ code/streamlit/get_data.py
@@ -9,5 +9,16 @@ def load_data():
     diagnose_2021 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Diagnose_2021.xlsx")
     diagnose_2022 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Diagnose_2022.xlsx")
     diagnose_2023 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Diagnose_2023.xlsx")
+    tode_2010 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2010.xlsx")
+    tode_2011 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2011.xlsx")
+    tode_2012 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2012.xlsx")
+    tode_2013 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2013.xlsx")
+    tode_2014 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2014.xlsx")
+    tode_2015 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2015.xlsx")
+    tode_2016 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2016.xlsx")
+    tode_2017 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2017.xlsx")
+    tode_2018 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2018.xlsx")
+    tode_2019 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2019.xlsx")
+    tode_2020 = pd.read_excel("../../data/cleaned_data/final_cleaned/Data_Final_Tode_2020.xlsx")
 
-    return d_krankenhaus, d_personal, diagnose_2018, diagnose_2019, diagnose_2020, diagnose_2021, diagnose_2022, diagnose_2023
+    return d_krankenhaus, d_personal, diagnose_2018, diagnose_2019, diagnose_2020, diagnose_2021, diagnose_2022, diagnose_2023, tode_2010, tode_2011, tode_2012, tode_2013, tode_2014, tode_2015, tode_2016, tode_2017, tode_2018, tode_2019, tode_2020
