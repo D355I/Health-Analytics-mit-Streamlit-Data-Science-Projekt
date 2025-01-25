e1da44d6f8d2d1a8ec3bc2e766dcade254701e7d
D355I <jonabach.kontakt@gmail.com>
2025-01-25 16:24:02 +0100

    bugfix

--- code/streamlit/pages/Entwicklung.py
+++ code/streamlit/pages/Entwicklung.py
@@ -11,7 +11,8 @@ scope = pd.read_excel("./assets/scope.xlsx", index_col=False)
 st.sidebar.subheader("Scope:")
 st.sidebar.write(scope)
 
-d_kh, d_pn, d_dg2018, d_dg2019, d_dg2020, d_dg2021, d_dg2022, d_dg2023, d_t2010, d_t2011, d_t2012, d_t2013, d_t2014, d_t2015, d_t2016, d_t2017, d_t2018, d_t2019, d_t2020, d_tode_insg  = load_data()
+
+d_kh, d_pn, d_dg2018, d_dg2019, d_dg2020, d_dg2021, d_dg2022, d_dg2023, d_t2010, d_t2011, d_t2012, d_t2013, d_t2014, d_t2015, d_t2016, d_t2017, d_t2018, d_t2019, d_t2020,d_t2021,d_t2022,d_t2023, d_tode_insg  = load_data()
 
 st.header("Entwicklung")
 st.write(" ")
